4884721220d1607b915b92d06b7669c27bc5c1c1
FreakyCoder <kurayogun@gmail.com>
2022-07-26 23:31:48 +0300

    fix: build

--- example/App.tsx
+++ example/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {View, Text, StatusBar, SafeAreaView, Pressable} from 'react-native';
 import AppleHeader from 'react-native-apple-header';
 import BottomSearchBar from 'react-native-bottom-search-bar';
-import BouncyCheckbox from './lib/BouncyCheckbox';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
 export default class App extends React.Component {
   render() {
